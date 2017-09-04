01cb01ead71cc5a49060de5ee9c6af2338403298
Kelly Stoops <kstoops815@gmail.com>
2017-09-04 00:59:28 -0500

    added navbar with bootstrap

--- main.js
+++ main.js
@@ -1,65 +1,26 @@
-var allBlogs = [];
-
-// var blog1 = {
-// 	title: "JavaScript, Anyone?",
-// 	content: "<p>Hello World! I have seen this so many times recently.  Why? To make sure my JavaScript file is successfully linked to my HTML file. This is the one of the first (but probably the single most important) lessons I have learned in my short tenure with NSS. If CSS and JS are not linked properly, then everything I have done with those languages is all for naught.</p><p>So let me back up a bit. I am in the beginning stages of a year-long, part-time evening bootcamp to become a software developer.  I literally never thought those words ever come out of my brain.  Ever.  I thought that was above my brain level.  All that gibberish I would see when I would accidentally, somehow, launch the inspector inside a web browser, looked like hieroglyphics.  An unknown type of communication.  And previously, it was.  Now, with 3 weeks of the bootcamp and 3 weeks of the NSS JumpStart class in my rear view mirror, I understand it more than I ever thought possible. I am not so arrogant to say that I understand even a quarter of it, but being able to read and understand even the smallest fraction of any code is more than I can say about myself two months ago.  I am proud of that!</p><p>Specifically about JavaScript, this is what I have learned so far.  It can make life as a developer work more efficiently, but it comes with mucho frustration.  I am definitely going to have to take JS one chunk at a time. We have recently focused on for loops.  Wow!  My first hurdle? Iteration. I had to figure out what in the world that word meant.  I can't say that I still fully understand it, but I now have a basic working knowledge of it.  It is a process that repeats until it is told to stop.  I may be right, and yet I may be wrong.  But that's what it means to me right now in this moment.  I also have learned that syntax will make or break your code. However, there are plenty of resources available to help you make sure your syntax is correct.  What really requires my focus is understanding how and why processes work, and when to implement them.  I feel like once I get that, like really get it, I will be cooking with gas!  Right now, I'm just trying to turn on the gas tank!</p>",
-// 	date: "July 26, 2017"
-// };
-
-// var blog2 = {
-// 	title: "I Don't Want to Fix It! I Want to Understand It!",
-// 	content: "<p>Five weeks. We have been in class now for five weeks. Already, I have questioned multiple times whether or not I am cut out for this, but every time I convince myself that I am. I just have to keep pushing forward, stretch my brain, and work until I understand it. Easier said than done. But, I am determined to do just that.</p> <p>A lot of people want to simply know how to fix it. At my current job, people ask me all the time to 'just fix it'! without understanding the why or how behind it. I am not that person. I question the hows and whys of everything. Maybe that's a good thing, maybe not. But it's me.</p> <p>The past couple of weeks, we have been working on functions in JavaScript. I have been able to make my code work, through the help of others, but I haven't quite figured out the hows and whys of functions. It just hasn't clicked yet. I feel like I am getting close to the <strong>ah-ha</strong> moment that I crave. But until then, I am continuing to work hard at trying to figure it out. I know that functions are at the very heart of JavaScript, so it is so important for me to get it to click. I can understand the basics, but when variable names are all over the place, I have a hard time figuring out what variable to put where, how and where to define all the variables, and when to call the function. I believe, though, that with more practice, I will see it. I don't just want to 'just do it', I want to understand it.</p>",
-// 	date: "August 7, 2017"
-// };
-
-
-// var blog3 = {
-// 	title: "Collaboration",
-// 	content: "<p>When I went to college, there was a building where all of the engineers/tech-y people had their classes. I did not have one class in that building the entire 4 years I was there. The group of people that frequented that building were always somewhat of a mystery to me. Most of them were loners, or at least I perceived them that way. It seemed like they were alone most of the time. I was a bit scared to talk to them because they seemed so much smarter than me. I feared I wouldn't be able to understand anything they said. Plus, I just assumed they preferred being alone, so I didn't bother them. For so many years, I have had the perception that computer programmers were loners who hid from everyone else, did their job, and then went home. Boy was I wrong!</p><p>Since the second week of this program, we have been in a study group with 3-4 other people. I have learned so much simply by talking through problems and concepts with my study group. This has truly been invaluable.</p><p>We recently completed our first group project. This really opened my eyes to how group-oriented software development truly is. We had to depend on everyone in the group to do their part so that we completed the project. If I didn't do my part, the work would have fallen on someone else, or the project wouldn't have been successful. We all had tasks like that. It was hard for me to not have control over every aspect of the project that my name was attached to. I have always had the mentality that if you want something done right, do it yourself. But what I found out is that when you learn to trust the people working with you, great things can happen. I absolutely loved the way our project turned out. I couldn't have created it on my own. We all had great ideas, and the end result was a culmination of all of them. I am learning more every day, and not only because of the instructors. It is largely because of my study group.</p><p>As I am in the thick of this program, I now know that software development is not a lonely profession. It is absolutely a team sport.",
-// 	date: "August 14, 2017"
-// };
-
-
-// var blog4 = {
-// 	title: "JavaScript, Listen Up!",
-// 	content: "<p>Hello? Can anyone hear me? Sometimes I feel like I am talking to my child when I am trying to create event listeners - I tell JavaScript to listen but it doesn't hear me! It seems that JavaScript has selective hearing, and I am trying to learn it's language!</p><p>I have really enjoyed learning about event listeners. I like the idea of telling my code to do something when something else happens. It feels like I am making magic! When I get it to work, I am ecstatic. It just seems so cool to watch it happen. However, I have been having a hard time with the syntax and where to put them in my code. When I write an event listener, it feels like a sad little puppy sitting all alone, waiting to be called. That really throws me for a loop. When event listeners are inside a function, I get it. But when they are hanging out as their own block of code, it feels out of place. Not routine.</p><p>I struggle with where to put them also. I understand that JS hoists functions to the top, but event listeners seem to just sit there. Because JavaScript feels so structured to me, it kind of throws me for a loop to have that code just sitting out there, waiting. But I do get the concept - here's a block of code that is listening for something to happen, such as a a mouse click, to execute. So in reality, JavaScript is a better listener than my kids, because it is forever listening. I just have to make sure I tell it in the correct format what to listen for, and what action I want it to take. JavaScript is all ears!",
-// 	date: "August 22, 2017"
-// };
-
-
-// var blog5 = {
-// 	title: "My Blog 5",
-// 	content: "Progress families inspire expose the truth data granular the overcome injustice initiative. Agile strategy inspiring changemaker empower communities humanitarian policymaker, gender rights human-centered. Global; thought leader inclusion activate; LGBTQ+ and. Collaborative consumption, engaging radical global fairness replicable. We must stand up; to; silo; the resistance compassion collective impact improve the world uplift. Unprecedented challenge, data, human-centered; white paper, social enterprise academic social impact. Capacity building to her body her rights grit humanitarian collaborative consumption. Society resilient improve the world; social innovation collaborate compelling agile mass incarceration. Corporate social responsibility a targeted resist paradigm. Compelling, low-hanging fruit triple bottom line, academic emerging; citizen-centered big data green space justice. Humanitarian humanitarian, initiative disrupt, collaborate deep dive game-changer, justice movements. Unprecedented challenge philanthropy, strategy low-hanging fruit effective altruism corporate social responsibility resilient our work. Boots on the ground indicators citizen-centered; dynamic disrupt living a fully ethical life inspiring. Inspirational shared value think tank.",
-// 	date: "July 22, 2017"
-// };
-
-// blogs.push(blog1);
-// blogs.push(blog2);
-// blogs.push(blog3);
-// blogs.push(blog4);
-// blogs.push(blog5);
+let allBlogs = [];
 
 function putMyBlogsInDom(){
 	var blogsData = JSON.parse(this.responseText);
-	console.log("blog posts", blogsData.blogs);
+	//console.log("blog posts", blogsData.blogs);
 	allBlogs = blogsData.blogs;
 	blogDomString(blogsData.blogs);
 }
 
-function ifLoadFails(){
+const ifLoadFails = () => {
 	console.log("it's broken");
 }
 
-var myBlogs = new XMLHttpRequest();
+const myBlogs = new XMLHttpRequest();
 myBlogs.addEventListener("load", putMyBlogsInDom);
 myBlogs.addEventListener("error", ifLoadFails);
 myBlogs.open("GET", "blogPosts.json");
 myBlogs.send();
 
-function blogDomString(myThoughts){
-	var domString = "";
-	for(var i = 0; i < myThoughts.length; i++) {
-		domString+=		`<article class="cards">`;
+const blogDomString = (myThoughts) => {
+	let domString = "";
+	for(let i = 0; i < myThoughts.length; i++) {
+		domString+=		`<article class="cards col-md-4">`;
 		domString+=		`<div class="title">`;
 		domString+=			`<h4>${myThoughts[i].title}</h4>`;
 		domString+=			`<span>${myThoughts[i].date}</span>`;
@@ -70,10 +31,10 @@ function blogDomString(myThoughts){
 		domString+=		`</article>`;
 	}
 
-	console.log("I am on the blog page.", domString);
+	//console.log("I am on the blog page.", domString);
 	writeToDom(domString);
 }
 
-function writeToDom(entries){
+const writeToDom = (entries) => {
 	document.getElementById("blogHolder").innerHTML = entries;
 }
