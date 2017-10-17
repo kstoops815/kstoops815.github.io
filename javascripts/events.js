fd0ecdbc08631fa17ba99de0231644268e48008d
Kelly Stoops <kstoops815@gmail.com>
2017-10-17 11:07:40 -0500

    print full blog click event working

--- javascripts/events.js
+++ javascripts/events.js
@@ -3,13 +3,6 @@
 const dom = require("./dom");
 const blogs = require("./blogs");
 
-// document.getElementById("blogHolder").addEventListener("click", function(e){
-// 	console.log("click event", e);
-// 	blogBox(e);
-// 	printSelectedBlog();
-// 	$("#entireBlog").removeClass("hidden");
-// 	});
-
 $("#blogHolder").click((e) => {
 	blogBox(e);
 	printSelectedBlog(e);
@@ -21,18 +14,10 @@ let selectedBlogCard;
 const blogBox = (e) => {
 	let target = $(e.target);
 	if(target.hasClass("child")){
-		//console.log("click event if", e);
 		selectedBlogCard = target.parent();
-		//console.log("click event if", selectedBlogCard);
 	} else if(target.parents().hasClass("cards")){
-		//console.log("first else if", e);
 		selectedBlogCard = target.parent().parent();
 	}
-	console.log("above return", selectedBlogCard);
-	return(selectedBlogCard);
-
-
-	//console.log("in blogBox function", selectedBlogCard);
 };
 
 const printSelectedBlog = () => {
